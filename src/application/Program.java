8f6929e06d9c6d134d670b9357b9ae74930d45d3
Bernardo <bernardomerlo49@gmail.com>
2023-09-05 11:39:30 -0300

    delete implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -2,6 +2,7 @@ package application;
 
 import java.util.Date;
 import java.util.List;
+import java.util.Scanner;
 
 import model.dao.DaoFactory;
 import model.dao.SellerDao;
@@ -11,6 +12,7 @@ import model.entities.Seller;
 public class Program {
 
 	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
 
 		SellerDao sd = DaoFactory.createSellerDao();
 
@@ -31,10 +33,11 @@ public class Program {
 			System.out.println(obj);
 		}
 
-		System.out.println("\n=== TEST 4: seller insert ===");
-		Seller sellerNovo = new Seller(null, "Greg", "greg@gmail.com", new Date(), 4000.00, department);
-		sd.insert(sellerNovo);
-		System.out.println("Inserted new Id = " + sellerNovo.getId());
+		//adiciona toda hora, só descomentar para inserir
+		//System.out.println("\n=== TEST 4: seller insert ===");
+		//Seller sellerNovo = new Seller(null, "Greg", "greg@gmail.com", new Date(), 4000.00, department);
+		//sd.insert(sellerNovo);
+		//System.out.println("Inserted new Id = " + sellerNovo.getId());
 
 		System.out.println("\n=== TEST 5: seller update ===");
 		seller = sd.findById(1);
@@ -42,6 +45,15 @@ public class Program {
 		sd.update(seller);
 		System.out.println("Update Complete");
 
+		System.out.println("\n=== TEST 6: seller delete ===");
+		System.out.print("Enter id for delete teste: ");
+		int id = sc.nextInt();
+		sd.deleteById(id);
+		System.out.println("Delete Complete");
+		
+		
+		sc.close();
+		
 	}
 
 }
--- src/model/dao/impel/SellerDaoJDBC.java
+++ src/model/dao/impel/SellerDaoJDBC.java
@@ -60,8 +60,7 @@ public class SellerDaoJDBC implements SellerDao {
 		PreparedStatement st = null;
 		try {
 			st = conn.prepareStatement("UPDATE seller "
-					+ "SET Name = ?, Email = ?, BirthDate = ?, BaseSalary = ?, DepartmentId = ? " 
-					+ "WHERE Id = ?",
+					+ "SET Name = ?, Email = ?, BirthDate = ?, BaseSalary = ?, DepartmentId = ? " + "WHERE Id = ?",
 					Statement.RETURN_GENERATED_KEYS);
 
 			st.setString(1, obj.getName());
@@ -81,8 +80,18 @@ public class SellerDaoJDBC implements SellerDao {
 
 	@Override
 	public void deleteById(Integer id) {
-		// TODO Auto-generated method stub
+		PreparedStatement st = null;
+		try {
+			st = conn.prepareStatement("DELETE FROM seller WHERE Id = ?");
 
+			st.setInt(1, id);
+			st.executeUpdate();
+
+		} catch (SQLException e) {
+			throw new DbException(e.getMessage());
+		} finally {
+			DB.closeStatement(st);
+		}
 	}
 
 	@Override
