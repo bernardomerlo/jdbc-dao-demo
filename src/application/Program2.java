287afe4f46cc00aeba97c062d10ed6616c9c20eb
Bernardo <bernardomerlo49@gmail.com>
2023-09-05 12:24:28 -0300

    findAll department implementation

--- src/application/Program2.java
+++ src/application/Program2.java
@@ -1,5 +1,8 @@
 package application;
 
+import java.util.List;
+import java.util.Scanner;
+
 import model.dao.DaoFactory;
 import model.dao.DepartmentDao;
 import model.entities.Department;
@@ -7,32 +10,36 @@ import model.entities.Department;
 public class Program2 {
 
 	public static void main(String[] args) {
-		
+		Scanner sc =new Scanner(System.in);
+
 		DepartmentDao dd = DaoFactory.createDepartmentDao();
-		
-		System.out.println("=== TEST 1: department insert ===");
-		Department department = new Department(null, "Video Games");
-		dd.insert(department);
-		System.out.println(department);
-		
-		System.out.println("\n=== TEST 2: department update ===");
-		department.setName("Video GAMES");
-		dd.update(department);
-		System.out.println(department);
-		
-		System.out.println("\n=== TEST 3: department delete ===");
-		Department dpTeste = new Department(null, "Teste");
-		dd.insert(dpTeste);
-		System.out.println(dpTeste);
-		dd.deleteById(dpTeste.getId());
-		
-		System.out.println("\n=== TEST 4: department findById ===");
-		Department dep = dd.findById(8);
+		System.out.println("=== TEST 1: findById =======");
+		Department dep = dd.findById(1);
 		System.out.println(dep);
 		
+		System.out.println("\n=== TEST 2: findAll =======");
+		List<Department> list = dd.findAll();
+		for (Department d : list) {
+			System.out.println(d);
+		}
+
+		System.out.println("\n=== TEST 3: insert =======");
+		Department newDepartment = new Department(null, "Music");
+		dd.insert(newDepartment);
+		System.out.println("Inserted! New id: " + newDepartment.getId());
+
+		System.out.println("\n=== TEST 4: update =======");
+		Department dep2 = dd.findById(1);
+		dep2.setName("Food");
+		dd.update(dep2);
+		System.out.println("Update completed");
 		
-		
-		
+		System.out.println("\n=== TEST 5: delete =======");
+		System.out.print("Enter id for delete test: ");
+		int id = sc.nextInt();
+		dd.deleteById(id);
+		System.out.println("Delete completed");
+
 	}
 
 }
